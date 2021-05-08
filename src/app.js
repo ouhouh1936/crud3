f572a59acabe00042585a5d144bfbc26aff98279
ouhouh1936 <ouhouh1936@gmail.com>
2021-05-08 15:50:20 +0900

    dfdfd

--- src/app.js
+++ src/app.js
@@ -4,9 +4,12 @@ import helmet from "helmet";
 import morgan from "morgan";
 import bodyParser from "body-parser";
 import boardRouter from "./routers/boardRouter";
+import connect from "../db/index";
 
 const app = express();
 const PORT = 3000;
+
+connect();
 
 app.use(helmet());
 app.use(morgan(`dev`));
--- src/controllers/boardController.js
+++ src/controllers/boardController.js
@@ -1,13 +1,65 @@
-export const homeController = (req, res) => {
+import Board from "../models/Board";
+
+export const homeController = async (req, res) => {
+  try {
+    const result = await Board.find().sort({ created: -1 });
+
+    res.render("home", { boardList: result });
+  } catch (e) {
+    console.log(e);
+    res.render("home", { boardList: [] });
+  }
   res.render("home");
 };
 
-export const detailController = (req, res) => {
+export const detailController = async (req, res) => {
+  const {
+    query: { id },
+  } = req;
+  try {
+    const result = await Board.findOne({ _id: id });
+
+    res.render("detail", { data: result });
+  } catch (e) {
+    console.log(e);
+    homeController(req, res);
+  }
+
   res.render("detail");
 };
 
 export const createController = (req, res) => {
   res.render("create");
+};
+
+export const createBoardController = async (req, res) => {
+  const {
+    body: { title, author, desc },
+  } = req;
+
+  try {
+    const D = new Date();
+    const year = D.getFullYear();
+    let month = D.getMonth() + 1;
+    let date = D.getDate();
+
+    month = month < 10 ? `0${month}` : month;
+    date = date < 10 ? `0${date}` : date;
+
+    const resultDate = `${year}-${month}-${date}`;
+
+    const result = await Board.create({
+      title: title,
+      description: desc,
+      author: author,
+      created: resultDate,
+    });
+
+    homeController(req, res);
+  } catch (e) {
+    console.log(e);
+    homeController(req, res);
+  }
 };
 
 export const editController = (req, res) => {
